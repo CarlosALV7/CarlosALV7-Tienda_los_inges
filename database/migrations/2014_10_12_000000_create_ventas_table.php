719b01979cd153e3d487409993c2f62e3fd0dc8d
Zayda Hasiel Vilchis Olvera <al222010577@gmail.com>
2021-12-05 10:03:19 -0600

    Tabla ventas

---
+++ database/migrations/2014_10_12_000000_create_ventas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('monto');
+            $table->decimal('iva');
+            $table->decimal('total');
+            $table->string('rol', 45);
+            $table->string('nombre_producto', 45);
+            $table->integer('cantidad', 3);
+            $table->decimal('costo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
